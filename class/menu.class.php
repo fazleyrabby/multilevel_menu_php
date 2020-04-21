3e658cf1542d3ce0b8ff732c2e32111313f9484e
unknown <fazley111@gmail.com>
2020-04-22 00:55:21 +0600

    updated

--- class/menu.class.php
+++ class/menu.class.php
@@ -1,25 +1,19 @@
 <?php 
     class Menu extends Db{
 
-        function main_menu($parent_id=0, $sub=''){
-            $icon = '';
+        function main_menu($parent_id=0){
             $sql = 'SELECT * from menu where parent='.$parent_id;
             $stmt = $this->query($sql);
             $row = $this->resultSet();
 
-            if ($sub == true) {
-                $icon = 'arrow';
-            }
-            
             if ($this->rowCount() > 0) {
                 foreach ($row as $key => $value) {
-                    $this->set_html($value, $icon);
+                    $this->set_html($value);
                 }
             }
         }
 
         function sub_menu($parent_id){
-            
             $sql = 'SELECT * from menu where parent='.$parent_id;
             $stmt = $this->query($sql);
             $row = $this->resultSet();
@@ -27,32 +21,24 @@
             return $rowCount;
         }
 
-        function set_html($menu, $icon){
-
-            if ($this->sub_menu($menu['id']) > 0) {
+        function set_html($menu){
+            $count = $this->sub_menu($menu['id']);
+            if ($count > 0) {
                 if ($menu['parent'] == 0) {
                     $icon = 'arrow-down';
                 }else{
                     $icon = 'arrow';
                 }
+                echo '<li><a href="#" class='.$icon.'>'.$menu['menu_name'].'</a><ul>';
+                    $this->main_menu($menu['id']);
+                echo '</ul></li>';
             }else{
-                $icon = '';
+                echo '<li><a href="#">'.$menu['menu_name'].'</a></li>';
             }
-
-            echo '<li><a href="#" class='.$icon.'>'.$menu['menu_name'].'</a>';
-            if($this->sub_menu($menu['id']) > 0 )
-            {   
-                echo '<ul>';
-                    $this->main_menu($menu['id'],true);
-                echo '</ul>';
-            }
-            echo '</li>';
             
         }
 
         public function show(){
             echo $this->main_menu();
         }
-
-
     }
